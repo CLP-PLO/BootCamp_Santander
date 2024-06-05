1c85dd385a560d133b99038c960d2c8946af4e5b
PauloF <plofer.rs@gmail.com>
2024-06-05 16:43:26 -0300

    Exemplos de Variaveis

---
+++ VariaveisClasse/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int idade = 25;
+        char cidade = "São Bernardo do Campo";
+        double cordenada = 65.154636;
+        boolean sexo = true;
+        final int ano_2000 = 2000;
+
+    }
+}
